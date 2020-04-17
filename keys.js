7553886927fc82353449e8b0e8fdce9e42e557dc
Yuriy <xett03@gmail.com>
2020-04-16 23:13:39 -0400

    folders created

---
+++ keys.js
@@ -0,0 +1,6 @@
+console.log("Spotify loaded 99%...");
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
---
+++ liri.js
@@ -0,0 +1,20 @@
+
+
+var axios = require("axios");
+
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=d1874160").then(
+  function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    }
+});
